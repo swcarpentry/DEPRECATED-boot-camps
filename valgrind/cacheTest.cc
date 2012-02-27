4dfb8a68e4668e7086104e50728216b6e8c38fac
Richard T. Guy <richardtguy84@gmail.com>
2012-02-27 11:18:03 +0100

    Added cache testing to valgrind example

--- valgrind/cacheTest.cc
+++ valgrind/cacheTest.cc
@@ -7,23 +7,34 @@ Tests cache misses.
 
 int main(int argc, char **argv)
 {
-  if (argc < 2){
-    printf("Usage: cacheTest sizeI sizeJ\n");
+  if (argc < 4){
+    printf("Usage: cacheTest fast sizeI sizeJ\nIn first input, use 1 for fast code (cache-smart) and anything else for slow (cache-poor) execution.\n");
     return 1;
   }
-  int sI = atoi(argv[1]);
-  int sJ = atoi(argv[2]);
-  int *arr = new int[sI*sJ]; // double array.
+  int runFast = atoi(argv[1]);
+  long sI = atoi(argv[2]);
+  long sJ = atoi(argv[3]);
 
+  printf("Operating on matrix of size %d by %d\n",sI,sJ);
+
+  long *arr = new long[sI*sJ]; // double array.
+
+  // El mejor!
   //for (int i=0; i < sI*sJ; ++i) arr[i] = i;
 
-  for (int i=0; i < sI; ++i)
-    for (int j=0; j < sJ; ++j)
-       arr[(i * (sJ)) + j ] = i;
+  if (runFast == 1){
 
-  for (int i=0; i < sI; ++i)
-    for (int j=0; j < sJ; ++j)
-       arr[(j * (sI)) + i ] = i;
+    for (long i=0; i < sI; ++i)
+      for (long j=0; j < sJ; ++j)
+         arr[(i * (sJ)) + j ] = i;
+
+  }else{
+
+    for (long i=0; i < sI; ++i)
+      for (long j=0; j < sJ; ++j)
+         arr[(j * (sI)) + i ] = i;
+
+  }
 
   return 1;
 }
