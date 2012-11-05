5fb39f87a1906264b9a040814d632a17b90b953b
C. Titus Brown <titus@idyll.org>
2012-11-05 12:04:17 -0500

    Fixed typo in project name

--- conf.py
+++ conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'SWC / SCripps / 2012-11'
+project = u'SWC / Scripps / 2012-11'
 copyright = u'2012, Software Carpentry'
 
 # The version info for the project you're documenting, acts as replacement for
