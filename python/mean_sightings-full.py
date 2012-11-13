2774e79ddaba9e081b0959b70d674c26bdebb61c
Justin Kitzes <jkitzes@berkeley.edu>
2012-11-13 14:34:25 -0800

    First draft of testing and scripting lesson

---
+++ python/mean_sightings-full.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+'''
+Module/script to calculate mean numbe of sightings of a given animal in a given 
+sightings csv file.
+'''
+
+import sys
+import matplotlib.mlab as ml
+import numpy as np
+
+
+def get_sightings(filename, focusanimal):
+
+    # Load table
+    tab = ml.csv2rec(filename)
+
+    # Standardize capitalization of focusanimal
+    focusanimal = focusanimal.capitalize()
+
+    # Find number of records and total count of animals seen
+    isfocus = (tab['animal'] == focusanimal)
+    totalrecs = np.sum(isfocus)
+
+    if totalrecs == 0:
+        meancount = 0
+    else:
+        meancount = np.mean(tab['count'][isfocus])
+
+    # Return num of records and animals seen
+    return totalrecs, meancount
+
+
+def get_sightings_loop(filename, focusanimal):
+
+    # Load table
+    tab = ml.csv2rec(filename)
+
+    # Loop through all records, countings recs and animals
+    totalrecs = 0
+    totalcount = 0
+    for rec in tab:
+        if rec['animal'] == 'Owl':
+            totalrecs += 1
+            totalcount += rec['count']
+
+    meancount = totalcount/totalrecs
+
+    # Return num of records and animals seen
+    return totalrecs, meancount
+
+if __name__ == '__main__':
+    #print sys.argv
+    filename = sys.argv[1]
+    focusanimal = sys.argv[2]
+    print get_sightings(filename, focusanimal)
