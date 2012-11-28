c532784dea4719a58af74f277b7a9baac43e737b
Azalee Bostroem <abostroem@gmail.com>
2012-11-27 23:08:27 -0500

    Modified python and practical to remove if __name__ == "__main__" Added directions to redirect output of practical to a file Modified practical.mk at Mike's suggestion Create presentation for python material

--- practical/practical.mk
+++ practical/practical.mk
@@ -1,10 +1,21 @@
 #This Makefile runs test_functions.py if calc_mean.py has been updated.
 #It will also run calc_mean.py on student_grades1.dat if student_grades1.dat has been updated
 
-test_functions.py : calc_mean.py
+#run nosetests always
+test : test_functions.py
 	nosetests test_functions.py
 
-calc_mean.py : student_grades1.dat
-	python calc_mean.py student_grades1.dat
+#Run nosetests if calc_mean.py or test_functions.py are updates
+nosetests.xml : test_functions.py calc_mean.py
+	nosetests test_function.py --with-xunit
+
+# If calc_mean.py is updated then mark test_functions.py as updated.
+test_functions.py : calc_mean.py
+	touch test_functions.py
+
+
+#Run calc_mean on student_gradesN.dat
+%.dat.avg : %.dat calc_mean.py
+	python calc_mean.py $<>$@
 
 
---
+++ python/find_area.py
@@ -0,0 +1,8 @@
+'''
+This file is used to call the area function in 
+the circle module to calculate the area of a circle
+'''
+import circle
+from circle import area
+
+print area(5)
