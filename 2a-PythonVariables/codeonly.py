5da8c7aac733e663477a34b2dcb800d0f9f307d8
Will Trimble <trimble@anl.gov>
2013-01-10 10:12:14 -0600

    Removed duplicate file; moved to Readme

---
+++ 2a-PythonVariables/codeonly.py
@@ -0,0 +1,130 @@
+print "hello world"
+# <codecell>
+help(int)
+# <codecell>
+int?
+# <codecell>
+dir(str)
+# <codecell>
+%run hello.py
+# <codecell>
+reset
+# <codecell>
+experiment = "current vs. voltage"
+# <codecell>
+print experiment
+# <codecell>
+voltage = 2
+# <codecell>
+current = 0.5
+# <codecell>
+print voltage, current
+# <codecell>
+type(experiment)
+# <codecell>
+type(voltage)
+# <codecell>
+type(current)
+# <codecell>
+type(voltage)
+# <codecell>
+voltage = 2.7
+# <codecell>
+type(voltage)
+# <codecell>
+voltage = "2.7 volts"
+# <codecell>
+type(voltage)
+# <codecell>
+voltage = 2
+# <codecell>
+voltageString = str(voltage)
+# <codecell>
+currentString = str(current)
+# <codecell>
+voltageString
+# <codecell>
+type(voltageString)
+# <codecell>
+resistanceString = "4.0"
+# <codecell>
+resistance = float(resistanceString)
+# <codecell>
+resistance
+# <codecell>
+type(resistance)
+# <codecell>
+resistanceString = "4.0 ohms"
+# <codecell>
+voltage = float(1)
+# <codecell>
+voltage = 1.
+# <codecell>
+type(voltage)
+# <codecell>
+a = 1
+# <codecell>
+b = 2
+# <codecell>
+c = a+b
+# <codecell>
+c
+# <codecell>
+type(a), type(b), type(c)
+# <codecell>
+a = 1
+# <codecell>
+b = 2.
+# <codecell>
+c = a + b
+# <codecell>
+c
+# <codecell>
+type(a), type(b), type(c)
+# <codecell>
+a = 2
+# <codecell>
+b = 3
+# <codecell>
+c = a * b
+# <codecell>
+c
+# <codecell>
+type(a), type(b), type(c)
+# <codecell>
+a = 1
+# <codecell>
+b = 2
+# <codecell>
+c = a / b
+# <codecell>
+c
+# <codecell>
+a = 1.
+# <codecell>
+b = 2.
+# <codecell>
+c = a / b
+# <codecell>
+c
+# <codecell>
+type(a), type(b), type(c)
+# <codecell>
+firstName = "Johann"
+# <codecell>
+lastName = "Gambolputty"
+# <codecell>
+fullName = firstName + lastName
+# <codecell>
+print fullName
+# <codecell>
+fullName = firstName + " " + lastName
+# <codecell>
+print fullName
+# <codecell>
+str.upper?
+# <codecell>
+fullName.upper()
+# <codecell>
+"Johann Gambolputty".upper()
+# <codecell>
