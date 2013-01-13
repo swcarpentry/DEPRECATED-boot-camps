debcf55cc2c7e4af85816804831f2bc9286bb014
Will Trimble <trimble@anl.gov>
2013-01-12 20:35:03 -0600

    updates to exercise markup

--- python/data_structures/2b-Exercises.py
+++ python/data_structures/2b-Exercises.py
@@ -14,9 +14,17 @@
 # 1. Make a list with 5 things in it.
 # <codecell>
   
+# 1. Add two more things.
+# <codecell>
+   
+# <markdowncell>
+# # Exercise 2
+# 1. Make a dictionary whose keys are the strings "zero" through "nine" and whose values are ints 0 through 9.
+# <codecell>
+digitsdict = {}
+#  Your code goes here
 
 # <markdowncell>
-
 # # Exercise 3
 # 1. Make a dictionary and experiment using different types as keys. Can containers be keys?
 
@@ -27,9 +35,15 @@
 
 # # Exercises 4
 # 1. Write an if statement that prints whether x is even or odd
+# <codecell>
+x=4
+print x
+if x < 5:
+  print "x is less than five!"
+else: 
+  print "x is not less than five!"
 
 # <codecell>
-  
 
 # <markdowncell>
 
@@ -48,8 +62,16 @@
 # <markdowncell>
 
 # # Exercise 7
-# 1. Read the file 'big_animals.txt' and print each line on which more than 10 moose were sighted.
-# 2. Turn the code for #1 into a function and use it on the files 'merida_animals.txt' and 'fergus_animals.txt'.
+# 7A. Read the file 'big_animals.txt' and print each line and the length of each line.
+# <codecell>
+
+# 7B. Read the file 'big_animals.txt' again and print each line where the number of animals sighted is greater than 10.  We'll won't output the lines with fewer than 10 animals.
+# <codecell>
+
+# 7C. Read the file 'big_animals.txt' again and return a list of tuples.
+# <codecell>
+
+# 7D. Turn the code for #7C into a function and use it on the files 'merida_animals.txt' and 'fergus_animals.txt'.
 
 # <codecell>
  
@@ -58,6 +80,4 @@
 # 1.  Check your answer to Exercise 5 (the value of 42!) using the **math.gamma()** function.
 # <codecell>
 import math
-
-
 
