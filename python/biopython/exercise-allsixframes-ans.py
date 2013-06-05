fbf9deb7fb408646bd994f5b9e24accaa25be085
Will Trimble <trimble@anl.gov>
2013-06-05 11:09:46 -0400

    added hint + answer for allsixframes

--- python/biopython/exercise-allsixframes-ans.py
+++ python/biopython/exercise-allsixframes-ans.py
@@ -9,13 +9,14 @@ def allsixframes(record):
     ''' This function takes a SeqRecord object and returns a list of 
     six strings, with the translation of the sequence in the seqrecord 
     in all six frames.'''
+#   your code goes here
     frame = []
     frame.append(record.seq.translate())  # translation
-    frame.append("NNN")  # translation + 1
-    frame.append("NNN")  # translation + 2
-    frame.append("NNN")  # reverse complement translation 
-    frame.append("NNN")  # reverse complement translation + 1 
-    frame.append("NNN")  # you should see the pattern by now
+    frame.append(record.seq[1:].translate())  # translation + 1
+    frame.append(record.seq[2:].translate())  # translation + 2
+    frame.append(record.seq.reverse_complement().translate())  # reverse complement translation 
+    frame.append(record.seq.reverse_complement()[1:].translate())  # reverse complement translation + 1 
+    frame.append(record.seq.reverse_complement()[2:].translate())  # you should see the pattern by now
     return frame
 
 #   Open a fastq file, goes through it record-by-record, and output
