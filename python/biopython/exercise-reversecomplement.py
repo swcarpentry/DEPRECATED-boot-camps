fb0fa208261696495894c334721412c5eff73cfe
Will Trimble <trimble@anl.gov>
2013-06-05 10:17:25 -0400

    syntax error in exercise-allsixframes

--- python/biopython/exercise-reversecomplement.py
+++ python/biopython/exercise-reversecomplement.py
@@ -5,13 +5,15 @@ in all six frames.  '''
 from Bio import SeqIO
 
 def reversecomplement(record):
-    ''' This function takes a SeqRecord object and returns its reverse complement'''
+    ''' This function takes a SeqRecord object and returns its 
+    reverse complement'''
 #   your code goes here
+    reversecomplementsequence = "N" * len(record.seq)
     return reversecomplementsequence 
+
 
 #   Open a fastq file, goes through it record-by-record, and output
 #   the sequence id, the sequence, and the translations 
-
 generator = SeqIO.parse("data/test-sequences.fasta", "fasta")
 for seqrecord in generator:
     reversesequence = reversecomplement(seqrecord)
